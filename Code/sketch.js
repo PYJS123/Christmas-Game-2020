2bf04b9fd4ea29316a6e6339f0d53e6f2bf7cc37
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2020-12-18 15:48:18 +0000

    Adding health

--- Code/sketch.js
+++ Code/sketch.js
@@ -4,6 +4,9 @@ let objs = [];
 let score = 0,
   time = 0,
   miss = 0;
+let lives = 3,
+  llives = 3;
+let last;
 
 function preload() {
   verdana = loadFont('7a9c453f-539d-4b06-8c84-fd90088c243d.TTF');
@@ -16,6 +19,9 @@ function setup() {
 
 function draw() {
   if ((reindeer.x < 50 || reindeer.x > width - 50) || (reindeer.y < 50 || reindeer.y > height - 50)) {
+    noLoop();
+  }
+  if (lives <= 0.001) {
     noLoop();
   }
   background(0, 255, 255);
@@ -34,7 +40,8 @@ function draw() {
         score++;
         continue;
       } else {
-        noLoop();
+        llives--;
+        objs.splice(i, 1);
       }
     }
     objs[i].update();
@@ -67,4 +74,12 @@ function draw() {
   textAlign(RIGHT, TOP);
   fill(100, 50, 0);
   text('Time: ' + time + " secs", width - 10, 10);
+  textAlign(CENTER, TOP);
+  fill(0, 0, 125);
+  text('Health:', width / 2, 10);
+  fill(0, 255, 255);
+  rect(width / 2 - 200 / 2, 50, 200, 30);
+  fill(0, 255, 0);
+  rect(width / 2 - 200 / 2, 50, 200 / 3 * lives, 30);
+  lives = lerp(lives, llives, 0.2);
 }
