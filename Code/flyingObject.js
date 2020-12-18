ce016f53380057a94862f24c2526fac56aa95803
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2020-12-18 13:23:42 +0000

    Update flyingObject.js

--- Code/flyingObject.js
+++ Code/flyingObject.js
@@ -1,5 +1,16 @@
 class UFO {
-  constructor(x, y) {
-    
+  constructor(x, y, bad) {
+    this.x = x;
+    this.y = y;
+    this.bad = bad;
+  }
+
+  show() {
+    fill(this.bad * 255);
+    circle(x, y, 50);
+  }
+  
+  update() {
+    this.x -= spd;
   }
 }
