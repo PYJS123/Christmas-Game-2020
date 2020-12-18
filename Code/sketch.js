d2965bd8b3231e0c393018a69177ff7c67fd86aa
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2020-12-18 13:07:13 +0000

    Give the reindeer controls

---
+++ Code/sketch.js
@@ -0,0 +1,24 @@
+let reindeer;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  reindeer = new Reindeer(width / 5, height / 2);
+}
+
+function draw() {
+  background(0, 255, 255);
+  reindeer.update();
+  reindeer.show();
+  if (keyIsDown(UP_ARROW)) {
+    reindeer.fy -= reindeer.spd;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    reindeer.fx += reindeer.spd;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    reindeer.fy += reindeer.spd;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    reindeer.fx -= reindeer.spd;
+  }
+}
